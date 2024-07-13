fd5f6de1940d87331213d36730bfe8990937afe3
rohanbhadange <rohanbhadange18@gmail.com>
2024-07-13 15:46:23 -0400

    Update Welcome.tsx
    Wingman

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,6 +1,6 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">✈️I am the Roles & Responsibilities Chatbot🚀</h1>
+    <h1 className="text-2xl font-bold mb-2">✈️Hello, I am Wingman, your Roles & Responsibilities Chatbot🚀</h1>
     <p>
       Ask any questions about the roles and responsibilities outlined in the official governance surrounding the Air Force Operations Department. 
     </p>
