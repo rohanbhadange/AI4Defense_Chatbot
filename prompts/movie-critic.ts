3e17f8e03fecd1b930c1d2c285b08570d2dc350c
rohanbhadange <rohanbhadange18@gmail.com>
2024-07-13 15:57:16 -0400

    Update movie-critic.ts
    new sample question

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -5,7 +5,7 @@ Do not answer questions that are not asking for a movie recommendations.
 If the user asks other questions, do not answer and instead deflect them with a movie fact or trivia.`;
 
 export const samplePhrases = [
-  "What are the best foodie movies?",
-  "Give me a famous movie as three emojis and I'll guess the title.",
-  "What should I watch if I loved Parasite?",
+  "What is the Air Force Surgeon General supposed to do in the context of biological warfare?",
+  "Give me the most important information about the LeMay Commander Center.",
+  "Who is involved in the Continuity of Operations (COOP) Program?",
 ];
