e7f7855fb7d5b25b0270290e839fff3800548649
rohanbhadange <rohanbhadange18@gmail.com>
2024-07-13 15:15:30 -0400

    Update Welcome.tsx
    changed default description to R&R

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,14 +2,9 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      Ask any questions about the roles and responsibilities outlined in the official governance surrounding the Air Force Operations Department. 
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
     </p>
   </div>
 );
