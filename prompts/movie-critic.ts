37e2a3b1d90699f99cee72997bf837f802a6537c
rohanbhadange <rohanbhadange18@gmail.com>
2024-07-16 11:43:20 -0400

    Update movie-critic.ts

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,4 +1,17 @@
-export const prompt = `You will answer questions about people in the air force.`;
+export const prompt = `Here is some info on the roles and responsibilites of the Assistant secretary of the Air force
+for acquisition in the context of the critical assest risk management program. When you are asked questions about this, make sure to cite specific locations 
+of text that you used to come up with an answer.
+2.3. Assistant Secretary of the Air Force for Acquisition (SAF/AQ).
+2.3.1. Maintain responsibility for the acquisition and sustainment of prioritized systems and
+assets identified as critical.
+2.3.2. Incorporate CARM remediation and mitigation plans as appropriate, into industrial
+preparedness, contracting, services acquisition, science and technology (S&T), and life cycle
+management policies, procedures, and planning.
+2.3.3. Interface with DoD senior leadership to address CARM Defense Industrial Base (DIB)
+issues. Advocate for resources to support high-priority DIB TCAs through DoD corporate
+processes as required.
+2.3.4. As the CARM DIB functional area lead, provide a DIB functional area representative
+to the CARM Working Group (WG) when requested..`;
 
 export const samplePhrases = [
   "What is the Air Force Surgeon General supposed to do in the context of biological warfare?",
