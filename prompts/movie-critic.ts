4d09de83b5cac00f32fc234077235442a52a275e
rohanbhadange <rohanbhadange18@gmail.com>
2024-07-16 13:34:43 -0400

    Update movie-critic.ts

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,6 +1,10 @@
-export const prompt = `Here is some info on the roles and responsibilites of the Assistant secretary of the Air force
-for acquisition in the context of the critical assest risk management program. When you are asked questions about this, make sure to cite specific locations 
-of text that you used to come up with an answer.
+export const prompt = `Here is some info on the roles and responsibilites of the Assistant secretary of the Air force for acquisition in the context of the critical assest risk management program. When you are asked questions about this, make sure to cite specific locations 
+of text that you used to come up with an answer. The content comes from this document: AIR FORCE INSTRUCTION 10-2402
+29 AUGUST 2017
+Operations
+CRITICAL ASSET RISK
+MANAGEMENT PROGRAM
+
 2.3. Assistant Secretary of the Air Force for Acquisition (SAF/AQ).
 2.3.1. Maintain responsibility for the acquisition and sustainment of prioritized systems and
 assets identified as critical.
@@ -11,7 +15,24 @@ management policies, procedures, and planning.
 issues. Advocate for resources to support high-priority DIB TCAs through DoD corporate
 processes as required.
 2.3.4. As the CARM DIB functional area lead, provide a DIB functional area representative
-to the CARM Working Group (WG) when requested..`;
+to the CARM Working Group (WG) when requested..
+
+The second document is about roles and responsibilites for the Air Combat command in this document's context: AIR FORCE INSTRUCTION 10-2606, BIOLOGICAL AGENT AEROSOL
+COLLECTION AND IDENTIFICATION
+2.3.2. Air Combat Command (ACC) will:
+6 AFI10-2606 6 JULY 2018
+2.3.2.1. Serve as Unit Type Codes (UTCs) Manpower and Equipment Force Packaging
+responsible agency for incorporating medical-related biological collection, detection, and
+identification resources into medical ground UTCs and Home Station Medical Response
+activities.
+2.3.2.2. Track and monitor for sustainment costs for the medical biological agent
+collection, detection and identification assets fielded on medical UTCs and Home Station
+Medical Response 886 allowance standards (Bioenvironmental Flight) assigned to
+medical units.
+2.3.2.3. Develop and sustain medical-related biological collection, detection, and
+identification equipment storage and deployment plan e.g., centralized storage, assets
+maintained at each AF installation.
+`;
 
 export const samplePhrases = [
   "What is the Air Force Surgeon General supposed to do in the context of biological warfare?",
